a70820d3edd236a6afb1cc4014fa6e70f5e04864
Miles Z. Sterrett <miles@mileszs.com>
2022-12-04 18:35:29 -0500

    Remove some cruft/noise

--- day-3/part-1/search.rb
+++ day-3/part-1/search.rb
@@ -1,14 +1,4 @@
-# Read lines (1 for each rucksack)
-# Split in half (1 half for each compartment)
-# Find characters in common between the two "compartments" -- Set?
-#
-# Total: 8240
-
 require_relative './list'
-
-CAPITAL_ADJUSTMENT = -38
-LOWERCASE_ADJUSTMENT = -96
-
 
 list = List.new
 list.process
